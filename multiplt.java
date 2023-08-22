cb2a3b1fdbeb0f398aab274067d4574382784ff8
Akshay Panchal <panchalakshay6277@gmail.com>
2023-08-22 20:42:50 +0530

    add  multiply.java

---
+++ multiplt.java
@@ -0,0 +1,8 @@
+`public class Sum{
+    public static void main(String[] args){
+        int a = 4;
+        int b = 5;
+        int muk = a * b;
+        System.out.println("The mul of a and b is: " + mul);
+    }
+}
