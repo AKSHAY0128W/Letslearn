01bd2d2baa2a52649a43c371cf7595c95378bc0d
Akshay Panchal <panchalakshay6277@gmail.com>
2023-08-23 20:22:14 +0530

    add implementation in diff java

---
+++ diff.java
@@ -0,0 +1,10 @@
+public  class diff
+{
+    public static void main(String args[])
+    {
+        int a = 4;
+        int b = 5;
+        int diff = a - b;
+        System.out.println("The diff of a and b is: " + diff);
+    }
+}
